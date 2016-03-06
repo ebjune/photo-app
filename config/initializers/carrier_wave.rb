fe92f900aaf4ab8f01604f070393a6bb9263640e
Erika Bjune <erika.bjune@gmail.com>
2016-03-06 23:14:07 +0000

    Still tweaking the aws S3 stuff

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('S3_BUCKET')
+    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
     config.aws_acl    = 'public-read'
 
    config.aws_credentials = {
-     access_key_id:     ENV.fetch('S3_ACCESS_KEY'),
-     secret_access_key: ENV.fetch('S3_SECRET_KEY'),
+     access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
      region:            ENV.fetch('AWS_REGION') # Required 'Oregon'
    }
 
