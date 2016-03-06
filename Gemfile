0e39d9d8b7b8db0e5b7582f5572801beebc4adfa
Erika Bjune <erika.bjune@gmail.com>
2016-03-06 22:34:48 +0000

    Swapped out fog for carrierwave-aws gem

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
-gem 'carrierwave'
+gem 'carrierwave-aws'
 gem 'mini_magick'
 gem 'fog'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,20 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-      }
-    config.fog_directory     = ENV['S3_BUCKET']
+    config.storage    = :aws
+    config.aws_bucket = ENV.fetch('S3_BUCKET')
+    config.aws_acl    = 'public-read'
+    
+   config.aws_credentials = {
+     access_key_id:     ENV.fetch('S3_ACCESS_KEY_ID'),
+     secret_access_key: ENV.fetch('S3_SECRET_KEY'),
+     region:            ENV.fetch('AWS_REGION') # Required 'Oregon'
+   }
+
+   # config.fog_credentials = {
+   #   :provider              => 'AWS',
+   #   :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+   #   :aws_secret_access_key => ENV['S3_SECRET_KEY']
+   #   }
+   # config.fog_directory     = ENV['S3_BUCKET']
   end
 end
